94d2dad99a5f57aa8a49403e2bdb58bdb4081d34
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-11-08 00:33:03 +0530

    fix::> Added 3 more test

--- test/utils/JsonParser.t.sol
+++ test/utils/JsonParser.t.sol
@@ -16,18 +16,28 @@ contract JsonParserTest is Test {
     //     helperSetToken(10);
     // }
 
-    function testInit() public {
-        helperSetToken(10);
+    /////////////
+    // Helpers //
+    /////////////
+    modifier helperSetToken(uint256 initSize) {
+        JsonParser.Token[] memory tempTokens;
+        (parser, tempTokens) = JsonParser.init(initSize);
 
+        delete tokens;
+        for (uint i = 0; i < tempTokens.length; i++) {
+            tokens.push(tempTokens[i]);
+        }
+        _;
+    }
+
+    function testInit() public helperSetToken(10) {
         assertEq(parser.pos, 0);
         assertEq(parser.toknext, 0);
         assertEq(parser.toksuper, -1);
         assertEq(tokens.length, 10);
     }
 
-    function testAllocateTokenSuccess() public {
-        helperSetToken(10);
-
+    function testAllocateTokenSuccess() public helperSetToken(10) {
         (bool success, JsonParser.Parser memory p, JsonParser.Token memory token) = JsonParser.allocateToken(
             parser,
             tokens
@@ -44,17 +54,13 @@ contract JsonParserTest is Test {
         assertEq(p.toknext, 1);
     }
 
-    function testAllocateTokenFail() public {
-        helperSetToken(1);
-
+    function testAllocateTokenFail() public helperSetToken(1) {
         parser.toknext = 1; // Full capacity
         (bool success, , ) = JsonParser.allocateToken(parser, tokens);
         assertFalse(success);
     }
 
-    function testFillToken() public {
-        helperSetToken(10);
-
+    function testFillToken() public helperSetToken(10) {
         JsonParser.Token memory token;
         token = JsonParser.fillToken(token, JsonParser.JsonType.STRING, 0, 5);
         assertEq(abi.encode(token.jsonType), abi.encode(JsonParser.JsonType.STRING));
@@ -64,16 +70,40 @@ contract JsonParserTest is Test {
         assertEq(token.end, 5);
     }
 
-    /////////////
-    // Helpers //
-    /////////////
-    function helperSetToken(uint256 initSize) public {
-        JsonParser.Token[] memory tempTokens;
-        (parser, tempTokens) = JsonParser.init(initSize);
-
-        delete tokens;
-        for (uint i = 0; i < tempTokens.length; i++) {
-            tokens.push(tempTokens[i]);
-        }
+    function testParseStringSuccess() public helperSetToken(10) {
+        bytes memory json = '"hello"';
+        parser.pos = 0;
+        (uint256 result, JsonParser.Parser memory parzer, JsonParser.Token memory token) = JsonParser.parseString(
+            parser,
+            tokens,
+            json
+        );
+        assertEq(result, JsonParser.RETURN_SUCCESS);
+        assertEq(parzer.pos, 6);
+        assertEq(token.start, 1);
+        assertEq(token.end, 6);
     }
+
+    function testParseStringInvalidJson() public {
+        bytes memory json = '"hello\\q"';
+        parser.pos = 0;
+        (uint256 result, , ) = JsonParser.parseString(parser, tokens, json);
+        assertEq(result, JsonParser.RETURN_ERROR_INVALID_JSON);
+    }
+
+    function testParseStringPartError() public {
+        bytes memory json = '"hello';
+        parser.pos = 0;
+        (uint256 result, , ) = JsonParser.parseString(parser, tokens, json);
+        assertEq(result, JsonParser.RETURN_ERROR_PART);
+    }
+
+    // function testParsePrimitiveSuccess() public helperSetToken(10) {
+    //     bytes memory json = "12345";
+    //     parser.pos = 0;
+    //     (uint256 result, , JsonParser.Token memory token) = JsonParser.parsePrimitive(parser, tokens, json);
+    //     assertEq(result, JsonParser.RETURN_SUCCESS);
+    //     assertEq(token.start, 0);
+    //     assertEq(token.end, 5);
+    // }
 }
