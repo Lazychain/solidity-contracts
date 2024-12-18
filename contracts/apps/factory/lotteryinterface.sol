b443d54f38a67b790a86a757036124e312686b6b
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-12-18 23:17:25 +0530

    fix::> changed handler base func signatures

--- contracts/apps/factory/lotteryinterface.sol
+++ contracts/apps/factory/lotteryinterface.sol
@@ -2,13 +2,13 @@
 pragma solidity ^0.8.24;
 
 interface INFTHandler {
-    function transferNFT(address from, address to, uint256 tokenId) external;
+    function transferNFT(address from, address to, uint256[] memory tokenIds, uint256[] memory amounts) external;
 
     function getMaxSupply() external view returns (uint256);
 
     function ownerOf(uint256 tokenId) external view returns (address);
 
-    function balanceOf(address user) external view returns (uint256);
+    function balanceOf(address user, uint256 tokenId) external view returns (uint256);
 
     function isApprovedForAll(address owner, address operator) external view returns (bool);
 }
